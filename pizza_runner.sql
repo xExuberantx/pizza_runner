18449fbcd3c773ec5f4d1a70fb1190231c3843e1
Kamil <131042937+xExuberantx@users.noreply.github.com>
2023-04-21 12:06:34 +0200

    Update pizza_runner.sql

--- pizza_runner.sql
+++ pizza_runner.sql
@@ -454,6 +454,7 @@ JOIN pizza_runner.pizza_toppings pt
 ON ou.exclusions=pt.topping_id OR ou.extras=pt.topping_id
 
 
+
 -- 5. Generate an alphabetically ordered comma separated ingredient list for each pizza order from the customer_orders table and add a 2x in front of any
 --    relevant ingredients
 --    > For example: "Meat Lovers: 2xBacon, Beef, ... , Salami"
